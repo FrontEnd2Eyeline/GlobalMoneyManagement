8840deaf3b1ac1b82de96743e8099beb73fe8358
Freddie8606 <ing_julianmadrid@hotmail.com>
2019-01-16 15:01:31 -0500

    Cambios Footer

---
+++ src/components/layouts/KYC.vue
@@ -0,0 +1,21 @@
+<template>
+    <el-container>
+      <div>
+        <el-main>
+          <div>
+
+          </div>
+        </el-main>
+      </div>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        name: "KYC"
+    }
+</script>
+
+<style scoped>
+
+</style>
