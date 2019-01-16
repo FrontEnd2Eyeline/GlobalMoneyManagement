f5d01f4ce8ddf2ec22f610fd99c2e475b5ac4531
Freddie8606 <ing_julianmadrid@hotmail.com>
2019-01-16 11:47:36 -0500

    Footer
    Va Footer +

--- src/components/components/Menu.vue
+++ src/components/components/Menu.vue
@@ -1,5 +1,43 @@
 <template>
-    
+    <el-container>
+      <div>
+        <el-aside>
+          <el-menu>
+            <el-menu-item-group>
+              <el-menu-item index="1">
+                <img src="../../assets/Imgs/logo_GMM.png" class="logoGMM">
+              </el-menu-item>
+              <el-menu-item index="2">
+                LOGIN &nbsp;|
+              </el-menu-item>
+              <el-menu-item index="3">
+                SIGN UP
+              </el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group justify="end">
+              <el-menu-item index="4">
+                INICIO
+              </el-menu-item>
+              <el-menu-item index="5">
+                LEGALES
+              </el-menu-item>
+              <el-menu-item index="6">
+                NOTICIAS
+              </el-menu-item>
+              <el-menu-item index="7">
+                FEEDBACK
+              </el-menu-item>
+              <el-menu-item index="8">
+                CONTACTO
+              </el-menu-item>
+              <el-menu-item index="9">
+                ESPAÑOL
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-menu>
+        </el-aside>
+      </div>
+    </el-container>
 </template>
 
 <script>
@@ -9,5 +47,7 @@
 </script>
 
 <style scoped>
-
+  .logoGMM{
+    width: 2.5em;
+  }
 </style>
