f079e13415f844c88f7f906c71239f10340a926c
Freddie8606 <ing_julianmadrid@hotmail.com>
2019-01-16 16:07:08 -0500

    Menu
    Menu opciones

--- src/components/components/Menu.vue
+++ src/components/components/Menu.vue
@@ -1,42 +1,89 @@
 <template>
     <el-container>
-      <div>
-        <el-aside>
-          <el-menu>
-            <el-menu-item-group>
-              <el-menu-item index="1">
-                <img src="../../assets/Imgs/logo_GMM.png" class="logoGMM">
-              </el-menu-item>
-              <el-menu-item index="2">
-                LOGIN &nbsp;|
-              </el-menu-item>
-              <el-menu-item index="3">
+      <el-main>
+        <div>
+          <el-row gutter="20">
+            <el-col :lg="3">
+              <img src="../../assets/Imgs/logo_GMM.png" class="logoGMM">
+            </el-col>
+            <el-col :lg="3">
+              <p class="colorMenu">
+                LOGIN
+              </p>
+            </el-col>
+            <el-col :lg="3">
+              <p class="colorMenu">
                 SIGN UP
-              </el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group justify="end">
-              <el-menu-item index="4">
+              </p>
+            </el-col>
+            <el-col :lg="3">
+              <p class="colorMenu">
                 INICIO
-              </el-menu-item>
-              <el-menu-item index="5">
+              </p>
+            </el-col>
+            <el-col :lg="3">
+              <p class="colorMenu">
                 LEGALES
-              </el-menu-item>
-              <el-menu-item index="6">
+              </p>
+            </el-col>
+            <el-col :lg="3">
+              <p class="colorMenu">
                 NOTICIAS
-              </el-menu-item>
-              <el-menu-item index="7">
+              </p>
+            </el-col>
+            <el-col :lg="3">
+              <p class="colorMenu">
                 FEEDBACK
-              </el-menu-item>
-              <el-menu-item index="8">
+              </p>
+            </el-col>
+            <el-col :lg="3">
+              <p class="colorMenu">
                 CONTACTO
-              </el-menu-item>
-              <el-menu-item index="9">
-                ESPAÑOL
-              </el-menu-item>
-            </el-menu-item-group>
-          </el-menu>
-        </el-aside>
-      </div>
+              </p>
+            </el-col>
+            <!--<el-col :lg="3">-->
+              <!--ESPAÑOL-->
+            <!--</el-col>-->
+          </el-row>
+          <el-row>
+            <el-col :span="4">
+              <img src="../../assets/Imgs/logo_GMM.png" class="logoGMM">
+            </el-col>
+            <el-col :span="20">
+              <el-tabs v-model="Menu" @tab-click="handleClick">
+                <el-tab-pane label="LOGIN" name="first">
+                  LOGIN
+                </el-tab-pane>
+                <el-tab-pane label="SIGN UP" name="second">
+                  SIGN UP
+                </el-tab-pane>
+                <el-tab-pane label="INICIO" name="third">
+                  INICIO
+                </el-tab-pane>
+                <el-tab-pane label="LEGALES" name="fourth">
+                  LEGALES
+                </el-tab-pane>
+                <el-tab-pane label="NOTICIAS" name="fifth">
+                  NOTICIAS
+                </el-tab-pane>
+                <el-tab-pane label="FEEDBACK" name="sixth">
+                  FEEDBACK
+                </el-tab-pane>
+                <el-tab-pane label="CONTACTO" name="seventh">
+                  CONTACTO
+                </el-tab-pane>
+                <el-tab-pane label="ESPAÑOL" name="eight">
+                  ESPAÑOL
+                </el-tab-pane>
+              </el-tabs>
+            </el-col>
+          </el-row>
+        </div>
+      </el-main>
+      <el-main>
+
+
+      </el-main>
     </el-container>
 </template>
 
@@ -47,7 +94,27 @@
 </script>
 
 <style scoped>
-  .logoGMM{
-    width: 2.5em;
+
+  @media only screen and (max-width: 1200px){
+    .logoGMM{
+      width: 2.5em;
+    }
+    .colorMenu{
+      color: #000 !important;
+      text-align: left;
+    }
   }
+
+  @media only screen and (min-width: 1201px)  {
+    .logoGMM{
+      width: 2.5em;
+      margin: 0 auto;
+      display: block;
+    }
+    .colorMenu{
+      color: #000 !important;
+      text-align: center;
+    }
+  }
+
 </style>
